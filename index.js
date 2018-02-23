45e74863060bd710f47aba44807f08472d368075
Alestainer <nikolaev.anton@phystech.edu>
2018-02-22 17:26:11 -1000

    clean index

--- index.js
+++ index.js
@@ -9,27 +9,6 @@ const scraper = new PropertyScraper(
   'badTMKs',
 );
 
-// (new Promise((resolve) => {
-//   scraper.getMultiUnitTMKs((condos) => {
-//     listMUTMKs(condos, resolve);
-//   });
-// })).then((tmks) => {
-//   scraper.scrapeCondosAsync(tmks, console.log);
-// });
-
 scraper.getMultiUnitTMKs().then(scraper.listMUTMKs).then((tmks) => {
   scraper.scrapeCondosAsync(tmks, 30).then(console.log).catch(console.log);
 });
-
-
-// scraper.getMultiUnitTMKs((condos) => {
-//   listMUTMKs(condos, (MUTMKs) => {
-//     const units = [];
-//     while (MUTMKs.length > 0) {
-//       scraper.scrapeOneCondo(MUTMKs.pop(), units.push);
-//     }
-//   });
-// });
-//
-// scraper.scrapeOneCondo(230170130000, console.log);
-// // scraper.getMultiUnitTMKs(console.log);
