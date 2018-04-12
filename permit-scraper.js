e3bb76a4dc5bb67f56d5bf53486a81c90fc525cc
Antonio <nikolaev.anton@phystech.edu>
2018-04-12 13:48:22 -1000

    Search page crawling

--- permit-scraper.js
+++ permit-scraper.js
@@ -1,9 +1,15 @@
 const casper = require('casper').create();
 
-casper.start('http://dppweb.honolulu.gov/DPPWeb/Default.aspx?PossePresentation=TaxMapKey&PosseObjectId=52896');
+casper.start(
+    'http://dppweb.honolulu.gov/DPPWeb/Default.aspx?PossePresentation=PropertySearch',
+    function () {
+    this.fillSelectors('span#TMK_713880_S0_sp', {
+        'input[name="TMK_713880_S0"]': '23017013'
+    }, false)
+});
 
 casper.then(function () {
-  casper.click('a#ctl00_cphTopBand_ctl03_hlkTabLink');
+  this.click('a#ctl00_cphBottomFunctionBand_ctl05_PerformSearch');
 });
 
 casper.then(function () {
