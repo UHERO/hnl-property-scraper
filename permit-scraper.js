c0e818213f9d761c6bfd83fb945c49e8801cdac9
Alestainer <nikolaev.anton@phystech.edu>
2018-04-13 13:55:40 -1000

    Permit scraper class structure

--- permit-scraper.js
+++ permit-scraper.js
@@ -2,23 +2,31 @@ const casper = require('casper').create();
 
 var PosseId = 0;
 
-casper.start(
-    'http://dppweb.honolulu.gov/DPPWeb/Default.aspx?PossePresentation=PropertySearch',
-    function () {
-    this.fillSelectors('span#TMK_713880_S0_sp', {
-        'input[name="TMK_713880_S0"]': '23017013'
-    }, false)
-});
+class PermitScraper {
 
-casper.then(function () {
-  this.click('a#ctl00_cphBottomFunctionBand_ctl05_PerformSearch');
-});
+  static openByTmk(tmk) {
+    casper.start(
+      'http://dppweb.honolulu.gov/DPPWeb/Default.aspx?PossePresentation=PropertySearch',
+      function () {
+        this.fillSelectors('span#TMK_713880_S0_sp', {
+          'input[name="TMK_713880_S0"]': tmk
+        }, false);
+      });
 
-casper.then(function () {
-    const addr = this.getCurrentUrl();
-    PosseId = addr.slice(addr.lastIndexOf('=') + 1);
-    this.echo(PosseId);
-});
+    casper.then(function () {
+      this.click('a#ctl00_cphBottomFunctionBand_ctl05_PerformSearch');
+    });
+    PermitScraper.getLinks();
+  }
+
+  static getLinks() {
+    casper.then(function () {
+      const addr = this.getCurrentUrl();
+      var PosseId = addr.slice(addr.lastIndexOf('=') + 1);
+      this.echo(PosseId);
+    });
+  }
+}
 
 casper.then(function () {
   this.echo(this.fetchText('span#AreaInSqFt_713924_38775_sp'));
