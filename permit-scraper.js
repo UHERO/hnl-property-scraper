e7988405fb63c7cc51f24e33085aa343e7dd00de
Antonio <nikolaev.anton@phystech.edu>
2018-04-13 18:16:21 -1000

    Basic info parsing

--- permit-scraper.js
+++ permit-scraper.js
@@ -1,40 +1,54 @@
-const casper = require('casper').create();
+var casper = require('casper').create();
 
-const PosseId = 0;
+var posseId = 0;
 
-class PermitScraper {
-  static parseByTmk(tmk) {
-    casper.start(
-      'http://dppweb.honolulu.gov/DPPWeb/Default.aspx?PossePresentation=PropertySearch',
-      function () {
-        this.fillSelectors('span#TMK_713880_S0_sp', {
-          'input[name="TMK_713880_S0"]': tmk,
-        }, false);
-      },
-    );
+var form = {};
 
-    casper.then(function () {
-      this.click('a#ctl00_cphBottomFunctionBand_ctl05_PerformSearch');
-    });
-
-    casper.then(function () {
-      const addr = this.getCurrentUrl();
-      const PosseId = addr.slice(addr.lastIndexOf('=') + 1);
-    });
-
-    casper.then(function () {
-      this.echo(this.fetchText('span#AreaInSqFt_713924_38775_sp'));
-    });
-
-    casper.then(function () {
-      this.click('a#ctl00_cphTopBand_ctl03_hlkTabLink');
-    });
-
-    casper.then(function () {
-      this.click('a#ctl00_cphTopBand_ctl04_hlkTabLink');
-    });
-
-    casper.run();
-  }
+function camelize(str) {
+    return str.trim().replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function (match, index) {
+        if (+match === 0) return ''; // or if (/\s+/.test(match)) for white spaces
+            return index === 0 ? match.toLowerCase() : match.toUpperCase();
+        });
 }
 
+casper.start(
+    'http://dppweb.honolulu.gov/DPPWeb/Default.aspx?PossePresentation=PropertySearch',
+    function () {
+        this.fillSelectors('span#TMK_713880_S0_sp', {
+            'input[name="TMK_713880_S0"]': '86023112',
+        }, false);
+    }
+);
+
+casper.then(function () {
+    this.click('a#ctl00_cphBottomFunctionBand_ctl05_PerformSearch');
+});
+
+casper.then(function () {
+    var addr = this.getCurrentUrl();
+    posseId = addr.slice(addr.lastIndexOf('=') + 1);
+});
+
+casper.then(function () {
+    form['developmentPlanAreas'] = this.fetchText('span[id^="Description_713925_734875"]');
+    form['floodZones'] = this.fetchText('span[id^="Description_713925_734356"]');
+    form['heightLimit'] = this.fetchText('span[id^="Description_713925_734869"]');
+    form['lotRestriction'] = this.fetchText('span[id^="Description_713925_775450"]');
+    form['neighborhoodBoards'] = this.fetchText('span[id^="Description_713925_775587"]');
+    form['SMA'] = this.fetchText('span[id^="Description_713925_734863"]');
+    form['slideArea'] = this.fetchText('span[id^="Description_713925_831366"]');
+    form['stateLandUse'] = this.fetchText('span[id^="Description_713925_734860"]');
+    form['streetSetback'] = this.fetchText('span[id^="Description_713925_775513"]');
+    form['zoning'] = this.fetchText('span[id^="Description_713925_734850"]');
+    this.echo(form.zoning);
+});
+
+casper.then(function () {
+    this.click('a#ctl00_cphTopBand_ctl03_hlkTabLink');
+});
+
+casper.then(function () {
+    this.click('a#ctl00_cphTopBand_ctl04_hlkTabLink');
+});
+
+casper.run();
